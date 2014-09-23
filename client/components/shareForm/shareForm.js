8467b346e426870fa5ef060df9ee5b673975084a
eblahm <matthew@webs.com>
2014-09-23 09:17:43 -0400

    reset form after submiting

--- client/components/shareForm/shareForm.js
+++ client/components/shareForm/shareForm.js
@@ -1,16 +1,34 @@
 
+var util = {
+	validate: function($form) {
+		if (!$form.find('[name="creator_name"]').val()) {
+			alert('Please provide a name')
+			return false;
+		}
+
+		if (!$form.find('[name="content"]').val()) {
+			alert('Please provide content to share');
+			return false
+		}
+		return true;
+	},
+
+	reset: function($form) {
+		var container = $form.closest('.share-form-container').get(0);
+		$form.remove();
+		Blaze.render(Template.shareForm, container);
+		$(".tags").select2({
+			placeholder: 'tags',
+			width: 'resolve'
+		});
+	}
+}
 
 Template.shareForm.events({
 	'click .submit button': function(event) {
 		var $form = $(event.currentTarget).closest('.share-form');
 
-		if (!$form.find('[name="creator_name"]').val()) {
-			return alert('Please provide a name');
-		}
-
-		if (!$form.find('[name="content"]').val()) {
-			return alert('Please provide content to share');
-		}
+		if (!util.validate($form)) return false;
 
 		var data = {
 			tags: [],
@@ -24,7 +42,9 @@ Template.shareForm.events({
 			}
 			data[item.name] = item.value;
 		});
-		collections.posts.insert(data);
+		collections.posts.insert(data, function() {
+			util.reset($form);
+		});
 	}
 });
 
