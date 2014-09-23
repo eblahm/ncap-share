0513ab3c7a3f9e20ebbde067f02fcdf2c705e7fc
eblahm <matthew@webs.com>
2014-09-23 09:02:19 -0400

    improve directory structure

--- client/components/landing/landing.js
+++ client/components/landing/landing.js
@@ -20,43 +20,10 @@ Template.landing.rendered = function() {
 
 
 Template.landing.events({
-	'click .submit button': function(event) {
-		var $form = $(event.currentTarget).closest('.share-form');
-
-		if (!$form.find('[name="creator_name"]').val()) {
-			return alert('Please provide a name');
-		}
-
-		if (!$form.find('[name="content"]').val()) {
-			return alert('Please provide content to share');
-		}
-
-		var data = {
-			tags: [],
-			files: [],
-			created: moment().toDate()
-		};
-		_.each($form.serializeArray(), function(item) {
-			if (item.name === 'tags') {
-				data.tags.push(item.value);
-				return;
-			}
-			data[item.name] = item.value;
-		});
-		collections.posts.insert(data);
-	}
 });
 
 
 Template.landing.helpers({
-
-	teamUnifyUser: function() {
-		return Session.get('teamUnifyUser');
-	},
-
-	tags: function() {
-		return collections.tags.find();
-	},
 
 	posts: function() {
 		return collections.posts.find().fetch();
---
+++ client/components/sharedItem/sharedItem.js
@@ -0,0 +1,12 @@
+
+
+
+Template.sharedItem.events({
+
+});
+
+
+Template.sharedItem.helpers({
+
+});
+
