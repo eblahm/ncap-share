16500a9b0c88c06337f1d557170adf8865f403cc
eblahm <matthew@webs.com>
2014-10-05 20:42:19 -0400

    loading indicator

--- client/router.js
+++ client/router.js
@@ -1,10 +1,12 @@
 
-
 Router.map(function() {
 
 	this.route('landing', {
 		path: '/',
-		waitOn: function() { return Meteor.subscribe('all'); }
+		waitOn: function() { return Meteor.subscribe('all'); },
+		action: function () {
+			this.ready()? this.render() : this.render('Loading');
+		}
 	});
 
 });
