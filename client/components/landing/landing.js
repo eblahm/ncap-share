4f6a3639ab3bfcaa31c94f4d5a25240b117093bb
eblahm <matthew@webs.com>
2014-10-11 23:21:12 -0400

    primative keyword search feature

--- client/components/landing/landing.js
+++ client/components/landing/landing.js
@@ -1,9 +1,24 @@
 
+var landingPage = {
 
-Template.landing.rendered = function() {
-	if(!this._rendered) {
-		this._rendered = true;
+	init: function() {
+		this.initAutocomplete();
+		this.listenForLogin();
+	},
 
+	initAutocomplete: function() {
+		var suggestions = _.map(collections.tags.find().fetch(), function(doc) {
+			return { value: doc.name, data: doc };
+		});
+		$('#search-posts').autocomplete({
+		    lookup: suggestions,
+		    onSelect: function (suggestion) {
+					Session.set('autocompleteTagSelection', suggestion.data);
+				}
+		});
+	},
+
+	listenForLogin: function() {
 		window.addEventListener('message', function(event) {
 			if (/teamunify\.com/.test(event.origin) || true) {
 				var data = JSON.parse(event.data);
@@ -17,10 +32,51 @@ Template.landing.rendered = function() {
 				});
 			}
 		}, false);
+	},
+
+	search: function() {
+		var keywordSearch = $('#search-posts').val();
+		var tagSelection = Session.get('autocompleteTagSelection');
+		var tagSearch = (tagSelection && tagSelection.name === keywordSearch);
+		var query;
+		if (tagSearch) {
+			query = {tags: tagSelection._id};
+		} else {
+			var regexsOnFields = _.map(['creator_name', 'content'], function(field) {
+				var subQuery = {};
+				subQuery[field] = {"$regex": keywordSearch, "$options": "i" }
+				return subQuery;
+			});
+			query = {$or: regexsOnFields};
+		}
+		Session.set('query', query);
+	}
+
+};
+
+
+Template.landing.rendered = function() {
+	if(!this._rendered) {
+		this._rendered = true;
+		landingPage.init();
 	}
 };
 
+
 Template.landing.events({
+	'keydown #search-posts': function(event) {
+		var ENTER_KEY = 13;
+		if (event.keyCode === ENTER_KEY) {
+			landingPage.search();
+		}
+	},
+
+	'click .search-label i': function(evt) {
+		var $tag = $(evt.currentTarget).closest('.search-label')
+		Session.set('query', {});
+		$tag.remove();
+	},
+
 });
 
 
@@ -34,6 +90,15 @@ Template.landing.helpers({
 	activeSearch: function() {
 		var query = Session.get('query') || {};
 		return !!_.keys(query).length;
+	},
+
+	appliedTags: function(){
+		var query = Session.get('query') || {};
+		return collections.tags.find({'_id': {$in: [query.tags]}});
+	},
+
+	query: function() {
+		return Session.get('query');
 	},
 
 });
